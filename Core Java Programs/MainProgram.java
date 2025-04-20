83e6a6a996c6a37d5f10ef59fecac49c2ac7b948
Abhishek Lanke <127180824+Abhilanke@users.noreply.github.com>
2025-04-20 14:39:34 +0530

    Update MainProgram.java

--- Core Java Programs/MainProgram.java
+++ Core Java Programs/MainProgram.java
@@ -4,26 +4,6 @@ public class MainProgram {
 
 	public static void main(String[] args) {
 		
-//		 Myinterface i=new  Myinterface();
-//		 {
-//			 
-//			 
-//		 }
-//		 i.sayHello();
-		
-		//Implementing interface using lambda expression/
-	    Myinterface mi=()->
-	    {
-	        System.out.println("Implementing interface using lambda expression");
-	    };
-	    mi.sayHello(); // calling function
-	    
-	    //Writing second implementation 
-	    Myinterface mi2=()->
-	    {
-	        System.out.println("Implementing interface using lambda expression second time");
-	    };
-	    mi2.sayHello(); // calling function
-	}
+System.out.println("Welcome To My Programs");
 
 }
